5000c7f1b633fa2bf4b59622467f74ad58f94f95
andrewhoon <hoonandrew1709@gmail.com>
2017-02-07 13:36:44 -0500

    next commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //added this
+using System.ComponentModel.DataAnnotations.Schema;  //added this also
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+
+        [ForeignKey("Category")]    
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }  //in category.cs, creating that field eliminated squiggly line under category
+
+
+
+
+
+
+    }
+}
